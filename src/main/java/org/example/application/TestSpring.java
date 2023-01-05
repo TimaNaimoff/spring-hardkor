f9452423e4ad988ddb91c7e3626912c540e21e1b
Temur Vegas <timanaimoff@gmail.com>
2023-01-05 17:08:43 +0500

    Dependency Injection + author's difficult tasks

--- src/main/java/org/example/application/TestSpring.java
+++ src/main/java/org/example/application/TestSpring.java
@@ -9,12 +9,13 @@ public class TestSpring {
     public static void main(String[]args){
         ClassPathXmlApplicationContext context=
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music=context.getBean("musicBean", Music.class);
-        Music musicall=context.getBean("musicallBean",Music.class);
-        MusicPlayer player=new MusicPlayer(music);
-        MusicPlayer player2=new MusicPlayer(musicall);
+        MusicPlayer player=context.getBean("musicPlayer",MusicPlayer.class);
         player.playMusic();
+
+        MusicPlayer player2=context.getBean("musicPlayerSetter",MusicPlayer.class);
         player2.playMusic();
+        System.out.println(player2);
         context.close();
+
     }
 }
--- src/main/java/org/example/music_domain/MusicPlayer.java
+++ src/main/java/org/example/music_domain/MusicPlayer.java
@@ -1,13 +1,69 @@
 package org.example.music_domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
-
+    private List<Music> list=new ArrayList<>();
+    private String name;
+    private int value;
     //IoC
-    public MusicPlayer(Music music){
-        this.music=music;
+    public MusicPlayer(Music music1,Music music2,Music music3){
+        list.add(music1);
+        list.add(music2);
+        list.add(music3);
     }
+    public MusicPlayer(){}
     public void playMusic(){
-        System.out.println(music.getSong());
+       list.forEach(e->{
+           System.out.println(e.getSong());
+       });
+    }
+
+    public void setList(Music...music) {
+        list.addAll(Arrays.asList(music));
+    }
+
+    public List<Music> getList() {
+        return list;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void setList(List<Music> list) {
+        this.list = list;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicPlayer{" +
+                "list=" + list +
+                ", name='" + name + '\'' +
+                ", value=" + value +
+                '}';
     }
 }
